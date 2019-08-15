b0d598178e46c354977a863e96ccb706dd3faeaa
Arina Pro <arina-pro@yandex.ru>
2019-08-15 18:30:08 +0500

    xor low-level api

---
+++ xor-tensorflow.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import numpy as np
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+X = tf.compat.v1.placeholder(tf.float32, shape=[4,2], name = 'X')
+Y = tf.compat.v1.placeholder(tf.float32, shape=[4,1], name = 'Y')
+
+W = tf.Variable(tf.random.uniform([2,2], -1, 1), name = "W")
+w = tf.Variable(tf.random.uniform([2,1], -1, 1), name = "w")
+
+c = tf.Variable(tf.zeros([4,2]), name = "c")
+b = tf.Variable(tf.zeros([4,1]), name = "b")
+
+with tf.name_scope("hidden_layer") as scope:
+    h = tf.nn.relu(tf.add(tf.matmul(X, W),c))
+
+with tf.name_scope("output") as scope:
+    y_estimated = tf.sigmoid(tf.add(tf.matmul(h,w),b))
+
+with tf.name_scope("loss") as scope:
+    loss = tf.reduce_mean(tf.math.squared_difference(y_estimated, Y))
+
+with tf.name_scope("train") as scope:
+    train_step = tf.compat.v1.train.GradientDescentOptimizer(0.01).minimize(loss)
+
+data = np.array([[0,0],[0,1],[1,0],[1,1]], "float32")
+
+labels = np.array([[0],[1],[1],[0]], "float32")
+
+init = tf.compat.v1.global_variables_initializer()
+
+sess = tf.compat.v1.Session()
+
+sess.run(init)
+
+for epoch in range(10001):
+    sess.run(train_step, feed_dict={X: data, Y: labels})
+    if epoch == 10000:
+        print('loss:', sess.run(loss, feed_dict={X: data, Y: labels}))
