c76eab8c55e07b43e1574364b0c028063d153c15
Arina Pro <arina-pro@yandex.ru>
2019-08-20 18:38:31 +0500

    keras model to train and save

--- xor-keras.py
+++ xor-keras.py
@@ -1,5 +1,7 @@
 import tensorflow as tf #Подключаем модуль Тензорфлоу
+from tensorflow.keras.models import load_model
 import numpy as np #Импортируем нампи для создания массивов
+import matplotlib.pyplot as plt #Импортируем модуль для визуализации
 
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR) #Убираем лишние предупреждения
 
@@ -18,9 +20,18 @@ data = np.array([[0,0],[0,1],[1,0],[1,1]], "float32") #4 разных входн
 
 labels = np.array([[0],[1],[1],[0]], "float32") #4 выхода, соответствующим входным данным
 
-model.fit(data, labels, epochs=10, verbose=2) #Обучение модели, 10 эпох, размер набора по дефолту 32, печатаем 1 линию для каждой эпохи
-model.save('my_model.h5')
-#model = tf.keras.models.load_model('my_model.h5')
-model.evaluate(data, labels, batch_size=1) #Оценка модели
+hist = model.fit(data, labels, epochs=10, verbose=2) #Обучение модели, 10 эпох, размер набора по дефолту 32, печатаем 1 линию для каждой эпохи
 
-print(model.predict(data, batch_size=1).round()) #Предсказание наученной моделью результатов, округление до единиц
+fig, axes = plt.subplots(2, sharex=True, figsize=(12, 8)) #Создаём 2 графика
+fig.suptitle('Training Metrics') #Подзаголовок
+
+axes[0].set_ylabel("Loss", fontsize=14) #Подписываем ось ординат 1 графика "Ошибка"
+axes[0].plot(hist.history['loss']) #Вносим наши значения ошибки в график
+axes[1].set_ylabel("Accuracy", fontsize=14) #Подписываем ось ординат 2 графика "Точность"
+axes[1].set_xlabel("Epoch", fontsize=14) #Подписываем ось абсцисс "Эпохи"
+axes[1].plot(hist.history['binary_accuracy']) #Вносим наши значения бинарной точности в график
+plt.show() #Визуализируем
+
+model.save('my_model.h5') #Сохраняем обученную модель
+
+model.evaluate(data, labels, batch_size=1) #Оценка модели
