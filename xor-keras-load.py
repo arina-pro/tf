1b86a82ba043ce5a2e45858412a706e28dbe45e5
Arina Pro <arina-pro@yandex.ru>
2019-08-21 17:06:47 +0500

    changed imports X2

--- xor-keras-load.py
+++ xor-keras-load.py
@@ -1,10 +1,10 @@
-import tensorflow as tf
+from tensorflow.compat.v1 import logging
 from tensorflow.keras.models import load_model
-import numpy as np
+from numpy import array
 
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+logging.set_verbosity(logging.ERROR)
 
 model = load_model('my_model.h5')
 
-test = np.array([[0,0],[0,1],[1,0],[1,1]], "float32")
+test = array([[0,0],[0,1],[1,0],[1,1]], "float32")
 print('For', test, 'model predicted', model.predict(test, batch_size=1).round())
