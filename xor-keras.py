fdf8b1cb9336792ad7045289e2c665816e433efa
Arina Pro <arina-pro@yandex.ru>
2019-08-21 17:06:20 +0500

    changed imports

--- xor-keras.py
+++ xor-keras.py
@@ -1,24 +1,26 @@
-import tensorflow as tf #Подключаем модуль Тензорфлоу
-from tensorflow.keras.models import load_model
-import numpy as np #Импортируем нампи для создания массивов
+from tensorflow.compat.v1 import logging #Импортируем из тф логи
+from tensorflow.keras import Sequential #Импортируем из кераса модель слоёв
+from tensorflow.keras.layers import Dense #Импортируем из кераса полносвязный слой
+from tensorflow.compat.v1.train import AdamOptimizer #Импортируем из тф оптимизатор адама
+from numpy import array #Импортируем из нампи массивы
 import matplotlib.pyplot as plt #Импортируем модуль для визуализации
 
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR) #Убираем лишние предупреждения
+logging.set_verbosity(logging.ERROR) #Убираем лишние предупреждения
 
-model = tf.keras.Sequential([ #Создаём модель последовательных слоёв для обучения
+model = Sequential([ #Создаём модель последовательных слоёв для обучения
 #Функция активации этого слоя для одномерных данных - выпрямитель
-tf.keras.layers.Dense(64, activation='relu', input_dim=2), #Второй слой принимает двухмерного инпут (вход+выход) и передаёт результаты 64 нейронам (спрятанный слой)
+Dense(64, activation='relu', input_dim=2), #Второй слой принимает двухмерного инпут (вход+выход) и передаёт результаты 64 нейронам (спрятанный слой)
 #Функция активации этого слоя для одномерных данных - сигмоида
-tf.keras.layers.Dense(1, activation='sigmoid')]) #Третий слой принимает инфу из 64 нейронов спрятанного слоя и выдаёт результат на 1 нейрон
+Dense(1, activation='sigmoid')]) #Третий слой принимает инфу из 64 нейронов спрятанного слоя и выдаёт результат на 1 нейрон
 
 #Добавляем конфигурацию к модели
-model.compile(optimizer=tf.compat.v1.train.AdamOptimizer(0.01), #Используем в нашей модели оптимизатор Адама с шагом в 0.01
+model.compile(optimizer=AdamOptimizer(0.01), #Используем в нашей модели оптимизатор Адама с шагом в 0.01
               loss='mse',       #Функция потери - среднее квадратичное ошибки
               metrics=['binary_accuracy'])  #Собираем метрику - бинарная точность, вероятность, что модель выберет "правильный" результат
 
-data = np.array([[0,0],[0,1],[1,0],[1,1]], "float32") #4 разных входных состояний в виде двухмерного массива
+data = array([[0,0],[0,1],[1,0],[1,1]], "float32") #4 разных входных состояний в виде двухмерного массива
 
-labels = np.array([[0],[1],[1],[0]], "float32") #4 выхода, соответствующим входным данным
+labels = array([[0],[1],[1],[0]], "float32") #4 выхода, соответствующим входным данным
 
 hist = model.fit(data, labels, epochs=10, verbose=2) #Обучение модели, 10 эпох, размер набора по дефолту 32, печатаем 1 линию для каждой эпохи
 
