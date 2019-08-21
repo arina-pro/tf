98405fbde97afa00562c699ca0148e17aa1b79c4
Arina Pro <arina-pro@yandex.ru>
2019-08-21 17:03:00 +0500

    added saver, wrapped graph

--- xor-tensorflow.py
+++ xor-tensorflow.py
@@ -1,40 +1,49 @@
-import tensorflow as tf #Импорт модуля ТФ
-import numpy as np #Импорт модуля нампи
+#Импорт модулей
+from tensorflow.compat.v1 import logging, placeholder, global_variables_initializer, Session
+from tensorflow import Variable, random, zeros, name_scope, nn, matmul, sigmoid, reduce_mean, Graph
+from tensorflow.compat.v1.train import GradientDescentOptimizer, Saver
+from numpy import array, square
 
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR) #Не показываем лишние предупреждения
+logging.set_verbosity(logging.ERROR) #Не показываем лишние предупреждения
 
-X = tf.compat.v1.placeholder(tf.float32, shape=[4,2], name = 'X') #Создаём объект для хранения входа
-Y = tf.compat.v1.placeholder(tf.float32, shape=[4,1], name = 'Y') #Создаём объект для хранения выхода
+with Graph().as_default(): #Открываем граф как главный
 
-W = tf.Variable(tf.random.uniform([2,2], -1, 1), name = "W") #Создаём переменную (наклон функции) с рандомным значением между -1 и 1 для входа
-w = tf.Variable(tf.random.uniform([2,1], -1, 1), name = "w") #Создаём переменную (наклон функции) с рандомным значением между -1 и 1 для выхода
+    X = placeholder("float32", shape=[4,2], name = 'X') #Создаём объект для хранения входа
+    Y = placeholder("float32", shape=[4,1], name = 'Y') #Создаём объект для хранения выхода
 
-c = tf.Variable(tf.zeros([4,2]), name = "c") #Создаём переменную (смещение по оси координат) с нулями для входа
-b = tf.Variable(tf.zeros([4,1]), name = "b") #Создаём переменную (смещение по оси координат) с нулями для выхода
+    W = Variable(random.uniform([2,2], -1, 1), name = "W") #Создаём переменную (наклон функции) с рандомным значением между -1 и 1 для входа
+    w = Variable(random.uniform([2,1], -1, 1), name = "w") #Создаём переменную (наклон функции) с рандомным значением между -1 и 1 для выхода
 
-with tf.name_scope("hidden_layer") as scope:
-    h = tf.nn.relu(tf.add(tf.matmul(X, W),c)) #Слой применяет "выпрямитель" к функции ((входные данные * W) + c)
+    c = Variable(zeros([4,2]), name = "c") #Создаём переменную (смещение по оси координат) с нулями для входа
+    b = Variable(zeros([4,1]), name = "b") #Создаём переменную (смещение по оси координат) с нулями для выхода
 
-with tf.name_scope("output") as scope:
-    y_estimated = tf.sigmoid(tf.add(tf.matmul(h,w),b)) #Слой применяет сигмоиду к функции ((предыдущий слой * w) + b)
+    with name_scope("hidden_layer") as scope:
+        h = nn.relu(matmul(X, W) + c) #Слой применяет "выпрямитель" к функции ((входные данные * W) + c)
 
-with tf.name_scope("loss") as scope:
-    loss = tf.reduce_mean(tf.math.squared_difference(y_estimated, Y)) #Слой считает среднее квадратичное ошибки между предыдущим слоем и выходными данными
+    with name_scope("output") as scope:
+        y_estimated = sigmoid(matmul(h,w) + b) #Слой применяет сигмоиду к функции ((предыдущий слой * w) + b)
 
-with tf.name_scope("train") as scope:
-    train_step = tf.compat.v1.train.GradientDescentOptimizer(0.01).minimize(loss) #Минимизируем ошибку при помощи метода градиентного спуска с шагом в 0.01 на каждом шаге
+    with name_scope("loss") as scope:
+        loss = reduce_mean(square(y_estimated - Y)) #Слой считает среднее квадратичное ошибки между предыдущим слоем и выходными данными
 
-data = np.array([[0,0],[0,1],[1,0],[1,1]], "float32") #Вход, массив нампи из 4 возможных конфигураций 0 и 1
+    with name_scope("train") as scope:
+        train_step = GradientDescentOptimizer(0.01).minimize(loss) #Минимизируем ошибку при помощи метода градиентного спуска с шагом в 0.01 на каждом шаге
 
-labels = np.array([[0],[1],[1],[0]], "float32") #Выход, массив нампи из 4 соответствующих результатов операции xor
+    data = array([[0,0],[0,1],[1,0],[1,1]], "float32") #Вход, массив нампи из 4 возможных конфигураций 0 и 1
 
-init = tf.compat.v1.global_variables_initializer() #Функция инициализации глобальных переменных
+    labels = array([[0],[1],[1],[0]], "float32") #Выход, массив нампи из 4 соответствующих результатов операции xor
 
-with tf.compat.v1.Session() as sess: #Открываем сессию
-    sess.run(init) #Инициализируем переменные
-    for epoch in range(10):
-        for element in range(32):
-            sess.run(train_step, {X: data, Y: labels}) #Обучаем модель на данных
-        #if (epoch + 1) % 10 == 0:
-        print('Epoch:', epoch + 1) #Эпоха обучения, каждые 32 шага
-        print('loss:', sess.run(loss, {X: data, Y: labels})) #Выводим ошибку после каждой эпохи обучения
+    init = global_variables_initializer() #Функция инициализации глобальных переменных
+
+    saver = Saver() #Метод сохранения переменных
+
+    with Session() as sess: #Открываем сессию
+        sess.run(init) #Инициализируем переменные
+        for epoch in range(10):
+            for element in range(32):
+                sess.run(train_step, {X: data, Y: labels}) #Обучаем модель на данных
+            #if (epoch + 1) % 10 == 0:
+            print('Epoch:', epoch + 1) #Эпоха обучения, каждые 32 шага
+            print('loss:', sess.run(loss, {X: data, Y: labels})) #Выводим ошибку после каждой эпохи обучения
+            save_path = saver.save(sess, "/tmp/model.ckpt") #Сохраняем чекпоинт каждую эпоху
+        print("Model saved in path: %s" % save_path) #Где найти сохранённую "модель"
